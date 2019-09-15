8db0865ccf0858e05793d8a74a7b0844cede725c
Ishraq Ahmad <ishraq.ahmad@probase.co.uk>
2019-09-15 10:49:06 -0700

    Added auth & config modules.

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Body, Post, ValidationPipe } from '@nestjs/common';
+import { AuthCredentialDto } from './dto/aut.credential.dto';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+
+    constructor(private authService: AuthService) {
+        //
+    }
+
+    @Post('/signup')
+    signUp(@Body(ValidationPipe) authCredentialDto: AuthCredentialDto): Promise<void> {
+        return this.authService.signUp(authCredentialDto);
+    }
+
+    @Post('/signIn')
+    signIn(@Body(ValidationPipe) authCredentialDto: AuthCredentialDto): Promise<{ accessToken: string }> {
+        return this.authService.signIn(authCredentialDto);
+    }
+}
